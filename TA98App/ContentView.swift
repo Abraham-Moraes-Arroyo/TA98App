c7edb27a23fdcf85601526e0c030d18337d1dcac
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-04-16 11:04:03 -0500

    Initial Commit

---
+++ TA98App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TA98App
+//
+//  Created by Abraham Morales Arroyo on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TA98App/TA98AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TA98AppApp.swift
+//  TA98App
+//
+//  Created by Abraham Morales Arroyo on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TA98AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
