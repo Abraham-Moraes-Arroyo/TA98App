c86170e2896d41c546e6fdfa5f63870d267f507d
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-04-16 11:48:47 -0500

    Added the additional files for the view in the correct directory.

---
+++ TA98App/Views/View1.swift
@@ -0,0 +1,18 @@
+//
+//  View1.swift
+//  TA98App
+//
+//  Created by Abraham Morales Arroyo on 4/16/24.
+//
+
+import SwiftUI
+
+struct View1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View1()
+}
