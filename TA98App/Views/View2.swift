2afc0634acb672146e9b56750fe70fc89d5e52ac
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-04-29 13:12:15 -0500

    Added the merch that we are going to be displaying on the second screen, and also added the reserve with us button,

--- TA98App/Views/View2.swift
+++ TA98App/Views/View2.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct View2: View {
     var body: some View {
-        Text("RSVP will take place here.")
+        VStack{
+            Text("Reserve your next Event")
+                .font(.title)
+            
+            
+        
+        
+        VStack{
+            // here is where we are going to have the button to contact TA98 and then we are going to have an image of the place. or something
+        Image("reserve")
+            .scaledToFit()
+            .frame(width: 400, height: 200)
+            .cornerRadius(65)
+            .frame(width: 10, height: 10)
+            .shadow(radius: 5)
+            .padding(2)
+            
+        
+            
+        }
+        .offset(y:100)
+            
+            
+            
+          
+        }
+        .offset(y:-300)
+        
+        VStack{
+            Button(action: {
+                if let url = URL(string: "https://www.ta98.org/contact") {
+                   UIApplication.shared.open(url)
+                }
+            }) {
+                Text("Reserve")
+                    .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 16)
+                                .stroke(.blue, lineWidth: 4)
+                        )
+            }
+        }
+        .offset(x:-100, y:-90 )
+   
     }
 }
 
