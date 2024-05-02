5be45d1962ab077c0a1a2aac6b0cf4993d1537db
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-05-02 14:55:49 -0500

    Created the screen and the folder group that will be used in making the be displayed inside of the app.

---
+++ TA98App/Online Gallery /OnlineArt.swift
@@ -0,0 +1,18 @@
+//
+//  OnlineArt.swift
+//  TA98App
+//
+//  Created by Abraham Morales Arroyo on 5/2/24.
+//
+
+import SwiftUI
+
+struct OnlineArt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnlineArt()
+}
