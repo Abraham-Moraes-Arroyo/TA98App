69c0f3a2026cc45823f1ceef22659390982fa644
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-05-06 18:44:08 -0500

    Added the lime green Scan painting and I have also added it to the view.

---
+++ TA98App/PracticeObjects/Home4.swift
@@ -0,0 +1,18 @@
+//
+//  Home4.swift
+//  TA98App
+//
+//  Created by Abraham Morales Arroyo on 5/6/24.
+//
+
+import SwiftUI
+
+struct Home4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home4()
+}
